839e86255f6216acf1970f4c2b5ae7f0ad2f64f9
Francesco Bartolomeo <116885550+Frankesko@users.noreply.github.com>
2024-08-29 10:38:57 +0200

    Update LeaderboardWithSorting.jsx

--- src/LeaderboardWithSorting.jsx
+++ src/LeaderboardWithSorting.jsx
@@ -83,23 +83,23 @@ const LeaderboardWithSorting = ({ data }) => {
                     {sortBy === 'ratioDesc' && <th>T/BW</th>}
                   </tr>
                 </thead>
-                <tbody>
-                  {items.map((item, index) => (
-                    <tr key={item.username}>
-                      <td className="position-name">
-                        <span className="position">{index + 1}.</span> {item.username}
-                      </td>
-                      {sortBy !== 'ratioDesc' && sortBy !== 'bwDesc' && <td>{item.s}</td>}
-                      {sortBy !== 'ratioDesc' && sortBy !== 'bwDesc' && <td>{item.b}</td>}
-                      {sortBy !== 'ratioDesc' && sortBy !== 'bwDesc' && <td>{item.d}</td>}
-                      {(sortBy === 'ratioDesc' || sortBy === 'bwDesc') && <td className="bw">{item.bw}</td>}
-                      <td className="total">{item.total}</td>
-                      {sortBy === 'ratioDesc' && (
-                        <td>{(item.total / parseFloat(item.bw)).toFixed(2)}</td>
-                      )}
-                    </tr>
-                  ))}
-                </tbody>
+               <tbody>
+                {items.map((item, index) => (
+                  <tr key={item.username}>
+                    <td className="position-name">
+                      <span className="position">{index + 1}</span> {item.username}
+                    </td>
+                    {sortBy !== 'ratioDesc' && sortBy !== 'bwDesc' && <td>{item.s}</td>}
+                    {sortBy !== 'ratioDesc' && sortBy !== 'bwDesc' && <td>{item.b}</td>}
+                    {sortBy !== 'ratioDesc' && sortBy !== 'bwDesc' && <td>{item.d}</td>}
+                    {(sortBy === 'ratioDesc' || sortBy === 'bwDesc') && <td className="bw">{item.bw}</td>}
+                    <td className="total">{item.total}</td>
+                    {sortBy === 'ratioDesc' && (
+                      <td>{(item.total / parseFloat(item.bw)).toFixed(2)}</td>
+                    )}
+                  </tr>
+                ))}
+              </tbody>
               </table>
             </React.Fragment>
           ))}
